90adaae8a75a013b7011feea5c98010227b248cd
Virgil Dupras <virgil.dupras@savoirfairelinux.com>
2014-02-05 11:36:06 -0500

    v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "openerp-sane",
-    version = "0.1",
+    version = "0.2",
     url = "https://github.com/savoirfairelinux/openerp-sane",
     py_modules = ['openerp_sane'],
     author="Savoir-faire Linux",
